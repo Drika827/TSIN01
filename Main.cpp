024e76a723eea34cfdb25f3fa022a50055faee94
Drika827 <74473792+Drika827@users.noreply.github.com>
2020-11-14 23:24:16 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -13,7 +13,7 @@ int main()
   //----Given variables----//
   const int Nodes = 100;
   const int Slots = 1000;
-  double ArrivalRate = (0.20);
+  double ArrivalRate = (0.35);
   double qr = 0.1;
 
 
@@ -27,22 +27,19 @@ int main()
   ofstream Output;
   string outcomeString;
 
-//  G(n) = (m−n)qa+nqr  Attempt rate
-//  Ps≈G(n)e^−G(n)
-
 
   //-------Simulator-------/
 
   Output.open("output.txt", ios::trunc);
 
 
-  /*
+  
   Output << " Slot "
              << " Backlog "
-      //       << " Arrivals "
-      //       << " Departures "
-      //       << " AttemptRate "
-      //       << " ProbSucc "
+             << " Arrivals "
+             << " Departures "
+             << " AttemptRate "
+             << " ProbSucc "
              << " ProbOfResend "
              << " estimatedBacklog "
              << " DelayAverage "
@@ -50,7 +47,7 @@ int main()
              << " ArrivalRate "
              << " Outcome"
              << endl;
-  */
+  
 
   for (int i = 0; i < Slots; ++i)
   {
@@ -64,14 +61,14 @@ int main()
     Output << setw(8) << setprecision(4) << SimData.backlog << " ";
     Output << setw(8) << setprecision(4) << TotalAmountOfArrivals << " ";
     Output << setw(8) << setprecision(4) << TotalAmountOfDepartures << " ";
-    //Output << setw(12) << setprecision(4) << SimData.AttemptRate << " ";
-    //Output << setw(12) << setprecision(4) << SimData.AveragePS << " ";
-    //Output << setw(12) << setprecision(4) << SimData.ProbOfResend << " ";
-    //Output << setw(12) << setprecision(4) << SimData.estimatedBacklog << endl;
+    Output << setw(12) << setprecision(4) << SimData.AttemptRate << " ";
+    Output << setw(12) << setprecision(4) << SimData.AveragePS << " ";
+    Output << setw(12) << setprecision(4) << SimData.ProbOfResend << " ";
+    Output << setw(12) << setprecision(4) << SimData.estimatedBacklog << " ";
     Output << setw(12) << setprecision(4) << SimData.DelayAverage << " ";
-    Output << setw(12) << setprecision(4) << SimData._w << endl;
-    //Output << setw(12) << setprecision(4) << SimData.ArrivalRate << " ";
-    //Output << setw(12) << SimData.outcome << endl;
+    Output << setw(12) << setprecision(4) << SimData._w << " ";
+    Output << setw(12) << setprecision(4) << SimData.ArrivalRate << " ";
+    Output << setw(12) << SimData.outcome << endl;
 
 
   }
